7ee507fdf96d2de843bab3ec2367bca4d6faa1da
Priate <ufoquinccy@outlook.com>
2021-12-29 01:25:24 +0800

    change reading model and add course model

---
+++ templates/_nuxt/4a78280.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[12],{433:function(t,e,n){"use strict";n.r(e);var r={data:function(){return{}},created:function(){var path=this.$route.query;this.$router.push(path.reload)}},o=n(55),component=Object(o.a)(r,(function(){var t=this.$createElement;return(this._self._c||t)("div")}),[],!1,null,null,null);e.default=component.exports}}]);
